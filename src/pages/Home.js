504392ef2c44778e48e71d3f7772da454df11d81
hamdy <hamdysameh480@gmail.com>
2025-11-17 20:25:25 +0200

    First commit - last work

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to Home Page</h1>
+      <p>دي الصفحة الرئيسية لموقعك.</p>
+    </div>
+  );
+}
---
+++ src/pages/Product.js
@@ -0,0 +1,8 @@
+export default function Product() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Our Products</h1>
+      <p>دي صفحة المنتجات.</p>
+    </div>
+  );
+}
